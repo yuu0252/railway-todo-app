aec845a7e37eab5909beb00a52dafd45ef19325d
yuu <yuu0252u@gmail.com>
2023-09-05 20:12:32 +0900

    station3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/* eslint no-undef: 0 */
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  /* eslint no-undef: 0 */
   expect(linkElement).toBeInTheDocument();
 });
--- src/const.js
+++ src/const.js
@@ -1,1 +1,2 @@
+/* eslint no-undef: 0 */
 export const url = process.env.REACT_APP_API_URL;
